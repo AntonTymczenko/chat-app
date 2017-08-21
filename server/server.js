fc9b681bf2faa50937934d0df1a62026988c4605
Anton Tymczenko <tymczenko@protonmail.com>
2017-08-21 18:52:23 +0300

    add connect and disconnect socket.io handlers

--- server/server.js
+++ server/server.js
@@ -1,15 +1,29 @@
 // dependencies:
 const path = require('path'),
-  express = require('express')
+  express = require('express'),
+  http = require('http'),
+  socketIO = require('socket.io')
 
 // configuration:
 require('dotenv').config()
 
-const app = express()
+const app = express(),
+  server = http.createServer(app),
+  io = socketIO(server)
+
+// middleware
 app.use(express.static(path.join(__dirname, '../public')))
 
 
+// server
 
-app.listen(process.env.PORT, ()=>{
+io.on('connection', (socket)=> {
+  console.log('new user connected')
+  socket.on('disconnect', ()=> {
+    console.log('user disconnected')
+  })
+})
+
+server.listen(process.env.PORT, ()=>{
   console.log(`Chat app started on port ${process.env.PORT} in ${process.env.NODE_ENV} mode`)
 })
